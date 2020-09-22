0e0dfaa17033c3a4049cdea90e48bdaca3ef73ee
YashKandalkar <yashsk.yk@gmail.com>
2020-09-23 00:19:48 +0530

    Updates

---
+++ precache-manifest.6b37df7207c8821a392600719376dc22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d713df209846f4c1f33c31a79b5b08",
+    "url": "/Huddle-landing-page/index.html"
+  },
+  {
+    "revision": "9aaeaab8611ab8d14d8d",
+    "url": "/Huddle-landing-page/static/css/main.d3b35d03.chunk.css"
+  },
+  {
+    "revision": "787000586acbe8a7574f",
+    "url": "/Huddle-landing-page/static/js/2.42543530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Huddle-landing-page/static/js/2.42543530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aaeaab8611ab8d14d8d",
+    "url": "/Huddle-landing-page/static/js/main.a5b199a6.chunk.js"
+  },
+  {
+    "revision": "b6a9824d9805ab55684b",
+    "url": "/Huddle-landing-page/static/js/runtime-main.a39acd6d.js"
+  }
+]);
